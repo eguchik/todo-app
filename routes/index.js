496823d3a4ab8fb45881fd6c263853ee1d9f0fb0
eguchik <k.eguchi84@gmail.com>
2022-02-15 16:47:26 +0900

    a

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const exec = require('child_process').exec;
 /* GET home page. */
 router.get("/", async function (req, res) {
   const tasks = await db.Task.findAll();
-  res.render("index", { title: "Anju", tasks });
+  res.render("index", { title: "Lise 減るエスタ", tasks });
 });
 
 router.post("/create", async function (req, res) {
